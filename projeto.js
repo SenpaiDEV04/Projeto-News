295bba721243296def032628498e24348add312c
SenpaiDEV04 <130495435+SenpaiDEV04@users.noreply.github.com>
2023-04-11 15:21:45 -0300

    Primeiro commit

---
+++ projeto.js
@@ -0,0 +1,19 @@
+$("#logs").focus(function(){
+    $(this).css("background-color","white");
+    $(this).css("transition",".3s");
+});
+
+$("#logs").blur(function(){
+    $(this).css("background-color","transparent");
+    $(this).css("transition",".3s");
+});
+
+$("#logs2").focus(function(){
+    $(this).css("background-color","white");
+    $(this).css("transition",".3s");
+});
+
+$("#logs2").blur(function(){
+    $(this).css("background-color","transparent");
+    $(this).css("transition",".3s");
+});
